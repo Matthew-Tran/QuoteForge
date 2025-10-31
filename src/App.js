57f85e77dbb970014962bdf475628718f78df827
Matthew Tran <99625196+Matthew-Tran@users.noreply.github.com>
2025-10-31 06:09:43 -0500

    Added StartingLogo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import StartingLogo from "./components/StartingLogo.jsx"
 
 function App() {
   return (
-     <h1>Hello World</h1>
+     <StartingLogo></StartingLogo>
   );
 }
 
---
+++ src/components/StartingLogo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function StartingLogo(){
+    return (
+        <h1 className="startinglogo"><span>Quote</span><span>Forge</span></h1>
+    );
+}
+
+export default StartingLogo;
